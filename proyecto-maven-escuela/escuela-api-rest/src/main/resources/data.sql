50819bfe303b7703b4f9cd6bc109db4874a9831a
adolfo.cia <adolfo.cia@tecso.coop>
2017-07-09 12:50:49 -0300

    fix data.sql

--- proyecto-maven-escuela/escuela-api-rest/src/main/resources/data.sql
+++ proyecto-maven-escuela/escuela-api-rest/src/main/resources/data.sql
@@ -1,1 +1,4 @@
+---------------------------------------------
+-- DDL para llenar la DB con datos de prueba |
+---------------------------------------------
 INSERT INTO NOVEDADES (nombre,descripcion) VALUES ('Acto','Acto realizado por todo el colegio');
