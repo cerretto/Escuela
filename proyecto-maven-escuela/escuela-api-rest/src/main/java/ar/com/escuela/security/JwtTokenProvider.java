cb7f1e67861e47e0d51a67d6e93abbdf34056325
adolfo.cia <adolfo.cia@tecso.coop>
2018-07-18 21:40:41 -0300

    seguridad terminada...

--- proyecto-maven-escuela/escuela-api-rest/src/main/java/ar/com/escuela/security/JwtTokenProvider.java
+++ proyecto-maven-escuela/escuela-api-rest/src/main/java/ar/com/escuela/security/JwtTokenProvider.java
@@ -13,9 +13,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import ar.com.escuela.exceptions.EscuelaRestErrorCode;
 import ar.com.escuela.exceptions.InvalidOrExpiredTokenException;
@@ -36,8 +39,8 @@ public class JwtTokenProvider {
 	@Value("${security.jwt.token.secret-key:secret-key}")
 	private String secretKey;
 
-	@Value("${security.jwt.token.expire-length:3600000}")
-	private long validityInMilliseconds = 3600000; // 1h
+	@Value("${security.jwt.token.expire-length}")
+	private long validityInMilliseconds;
 
 	@Autowired
 	private MyUserDetails myUserDetails;
@@ -47,10 +50,17 @@ public class JwtTokenProvider {
 		secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
 	}
 
+	/**
+	 * Creates the token with the username and a list of roles of the user, and
+	 * returns it.
+	 */
 	public String createToken(String username, List<Rol> roles) {
 
 		Claims claims = Jwts.claims().setSubject(username);
-		claims.put("auth", roles.stream().map(s -> new SimpleGrantedAuthority(s.getCodigo())).filter(Objects::nonNull).collect(Collectors.toList()));
+		List<GrantedAuthority> listGrantedRoles = roles.stream().map(s -> new SimpleGrantedAuthority(s.getCodigo()))
+				.filter(Objects::nonNull).collect(Collectors.toList());
+
+		claims.put("auth", listGrantedRoles);
 
 		Date now = new Date();
 		Date validity = new Date(now.getTime() + validityInMilliseconds);
@@ -63,15 +73,47 @@ public class JwtTokenProvider {
 				.compact();
 	}
 
+	/**
+	 * Method to be call on every request by the web filter JwtTokenFilter
+	 */
+	public void authenticate(HttpServletRequest req) {
+
+		String token = this.resolveToken((HttpServletRequest) req);
+
+		if (this.validateToken(token)) {
+
+			Authentication auth = this.getAuthentication(token);
+
+			SecurityContextHolder.getContext().setAuthentication(auth);
+		}
+	}
+
+	/**
+	 * Returns a standard Authentication object, filled with the authenticated user
+	 * data recovered on the database
+	 */
 	public Authentication getAuthentication(String token) {
 		UserDetails userDetails = myUserDetails.loadUserByUsername(getUsername(token));
 		return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
 	}
 
+	/**
+	 * Extracts the username from the token, using the secret key as part of the
+	 * decoding process.
+	 * 
+	 * @param token
+	 * @return
+	 */
 	public String getUsername(String token) {
 		return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
 	}
 
+	/**
+	 * Recover the token from the 'Authorization' header. <br>
+	 * Returns null if the 'Authorization' header is empty, or if it doesnt start
+	 * with 'Bearer '
+	 * 
+	 */
 	public String resolveToken(HttpServletRequest req) {
 		String bearerToken = req.getHeader("Authorization");
 		if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
@@ -80,7 +122,15 @@ public class JwtTokenProvider {
 		return null;
 	}
 
+	/**
+	 * Returns true if Token is valid. <br>
+	 * Checks against the secret key and expired time
+	 */
 	public boolean validateToken(String token) {
+
+		if (StringUtils.isEmpty(token))
+			return false;
+
 		try {
 			Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
 			return true;
--- proyecto-maven-escuela/escuela-api-rest/src/main/java/ar/com/escuela/security/MyUserDetails.java
+++ proyecto-maven-escuela/escuela-api-rest/src/main/java/ar/com/escuela/security/MyUserDetails.java
@@ -15,35 +15,32 @@ import ar.com.escuela.seg.service.SeguridadService;
 @Service
 public class MyUserDetails implements UserDetailsService {
 
-	  @Autowired
-	  private SeguridadService seguridadService;
+	@Autowired
+	private SeguridadService seguridadService;
 
-	  @Override
-	  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-	    final Usuario user = seguridadService.getUsuarioByUserName(username);
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		final Usuario user = seguridadService.getUsuarioByUserName(username);
 
-	    if (user == null) {
-	      throw new UsernameNotFoundException("User '" + username + "' not found");
-	    }
-	    
-	    List<String> rolesList = new ArrayList<>();
-	    user.getUsuarioRoles()
-	    	.stream()
-	    	.filter(x -> x != null)
-	    	.forEach(x -> {
-	    		rolesList.add(x.getRol().getCodigo());
-	    	});
-	    String[] arrayRoles = rolesList.toArray(new String[0]);
-	    
-	    return org.springframework.security.core.userdetails.User//
-	        .withUsername(username)//
-	        .password(user.getPassword())//
-	        .authorities(arrayRoles)//
-	        .accountExpired(false)//
-	        .accountLocked(false)//
-	        .credentialsExpired(false)//
-	        .disabled(false)//
-	        .build();
-	  }
+		if (user == null) {
+			throw new UsernameNotFoundException("User '" + username + "' not found");
+		}
+
+		List<String> rolesList = new ArrayList<>();
+		user.getUsuarioRoles().stream().filter(x -> x != null).forEach(x -> {
+			rolesList.add(x.getRol().getCodigo());
+		});
+		String[] arrayRoles = rolesList.toArray(new String[0]);
+
+		return org.springframework.security.core.userdetails.User//
+				.withUsername(username)//
+				.password(user.getPassword())//
+				.authorities(arrayRoles)//
+				.accountExpired(false)//
+				.accountLocked(false)//
+				.credentialsExpired(false)//
+				.disabled(false)//
+				.build();
+	}
 
 }
