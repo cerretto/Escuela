7224ac2d40f509530235fcad9636a30eba900183
Pablo <cerretto@hotmail.com>
2018-08-27 20:04:00 -0300

    Implementacion logout

--- escuela-frontend/src/app/menu/menu.component.ts
+++ escuela-frontend/src/app/menu/menu.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../security/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  salir() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
   }
 
 }
