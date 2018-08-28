6eb7f877d086bfe32dbcac906f57a818d759a9a0
Pablo <cerretto@hotmail.com>
2018-08-27 21:13:57 -0300

    Permisos por usuario

--- escuela-frontend/src/app/menu/menu.component.ts
+++ escuela-frontend/src/app/menu/menu.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../security/auth.service';
 import { Router } from '@angular/router';
+import { UserSession } from '../security/auth-model';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Component({
   selector: 'app-menu',
@@ -9,14 +11,29 @@ import { Router } from '@angular/router';
 })
 export class MenuComponent implements OnInit {
 
+  user: UserSession = new UserSession();
+
   constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
+    let token = localStorage.getItem('token');
+    let helper = new JwtHelperService();
+    let pepito = helper.decodeToken(token);
+    this.user.rol = pepito.auth[0].authority;
+    this.user.username = pepito.sub;
   }
 
   salir() {
     this.authService.logout();
     this.router.navigate(['/login']);
+  }
+
+  isAdmin() {
+    if (this.user.rol === 'ADMIN') {
+      return true;
+    } else {
+      return false;
+    }
   }
 
 }
