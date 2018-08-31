2e5346bf8d3b8c376c6845aa21c4b1130f3136cc
adolfo.cia <adolfo.cia@tecso.coop>
2018-08-31 19:55:43 -0300

    fix

--- escuela-frontend/src/app/security/auth-guard.service.ts
+++ escuela-frontend/src/app/security/auth-guard.service.ts
@@ -10,22 +10,23 @@ export class AuthGuard implements CanActivate {
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
 
+        if (!this.auth.isAuthenticated()) {
+            this.router.navigate(['/login']);
+            return false;
+        }
+
         if (route.routeConfig.path !== 'menu') {
             let token = localStorage.getItem('token');
             let helper = new JwtHelperService();
             let pepito = helper.decodeToken(token);
-    
+
             if (pepito.auth[0].authority !== 'ADMIN') {
                 this.router.navigate(['/login']);
                 return false;
             }
         }
 
-        if (!this.auth.isAuthenticated()) {
-            this.router.navigate(['/login']);
-            return false;
-        }
         return true;
     }
-    
+
 }
