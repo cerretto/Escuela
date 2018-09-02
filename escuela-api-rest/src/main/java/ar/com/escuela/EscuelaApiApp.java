9db5725ddfb2f611b452e1ec9e15a6a78b335d72
adolfo.cia <adolfo.cia@tecso.coop>
2018-09-02 13:40:44 -0300

    cambios

--- escuela-api-rest/src/main/java/ar/com/escuela/EscuelaApiApp.java
+++ escuela-api-rest/src/main/java/ar/com/escuela/EscuelaApiApp.java
@@ -1,15 +1,45 @@
 package ar.com.escuela;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ResourceProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+import java.io.IOException;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 public class EscuelaApiApp {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EscuelaApiApp.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EscuelaApiApp.class, args);
+    }
 
+    @Configuration
+    public class MyAppWebMvcConfigurer extends WebMvcConfigurerAdapter {
+
+        @Autowired
+        private ResourceProperties resourceProperties;
+
+        @Override
+        public void addResourceHandlers(ResourceHandlerRegistry registry) {
+            registry.addResourceHandler("/**/*")
+                    .addResourceLocations(resourceProperties.getStaticLocations())
+                    .resourceChain(true)
+                    .addResolver(new PathResourceResolver() {
+                        @Override
+                        protected Resource getResource(String resourcePath, Resource location) throws IOException {
+                            Resource requestedResource = location.createRelative(resourcePath);
+                            return requestedResource.exists() && requestedResource.isReadable() ? requestedResource : new ClassPathResource("/static/index.html");
+                        }
+                    });
+        }
+    }
 }
